a2a2490d2768da785747c2a42de939421bd6aa97
Alexander Wirt <formorer@grml.org>
2012-05-04 10:56:13 +0200

    use puppetforge ntpclass

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,5 @@
-class ntp {
-
-    package { 'ntp':
-        ensure => present
-    }
-
-    file { '/etc/ntp.conf':
-        ensure => present,
-        owner => root,
-        group => root,
-        mode => '0644'
-    }
-
-    service { 'ntp':
-        ensure => running,
-        hasstatus => true,
-        hasrestart => true,
-        require => Package['ntp']
-    }
+class ntp-hiera {
+        class { 'ntp':
+                 server_list => hiera('ntpservers')
+        }
 }
