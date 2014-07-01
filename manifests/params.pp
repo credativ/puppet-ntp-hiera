c2081cdd27059bac382399678b98465c34d00889
Arnd Hannemann <arnd@arndnet.de>
2014-07-01 13:13:13 +0200

    fix case where ntpservers isn't set (but ntp-hiera_server_list)

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class ntp-hiera::params {
     # backwards compability
-    $server_list = hiera('ntpservers')
+    $server_list = hiera('ntpservers', undef)
 
     # for other params use default of ntp class
     $interface_ignore = undef
